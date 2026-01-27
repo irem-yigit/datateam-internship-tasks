f83aaa540f69d3fc79dc885afff60ec9f5803058
irem-yigit <yigitiremm@gmail.com>
2026-01-27 18:27:59 +0300

    chore: update system files

--- backend/BookerApp/obj/Debug/net10.0/BookerApp.AssemblyInfo.cs
+++ backend/BookerApp/obj/Debug/net10.0/BookerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebd3831246c086002f28630020069d004028c12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c383a5e681aa57784a55d734acc937dd8bebafcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
