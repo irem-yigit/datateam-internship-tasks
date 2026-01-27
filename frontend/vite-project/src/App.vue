ebd3831246c086002f28630020069d004028c12d
irem-yigit <yigitiremm@gmail.com>
2026-01-27 11:15:07 +0300

    feat: fetch dummy data with axios

--- frontend/vite-project/src/App.vue
+++ frontend/vite-project/src/App.vue
@@ -1,11 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const posts = ref([])
+const loading = ref(true)
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    posts.value = response.data
+    console.log('Fetched posts:', posts.value)
+  } catch (error) {
+    console.error('Error fetching posts:', error)
+  }
+  finally {
+    loading.value = false
+  }
+})
 </script>
 
 <template>
-  <HelloWorld msg="Hello World" />
+  <pre class="json">
+  {{ JSON.stringify(posts, null, 2) }}
+  </pre>
 </template>
 
-<style scoped>
-  
+<style>
+.json {
+white-space: pre-wrap;
+word-break: break-word;
+font-family: monospace;
+padding: 16px;
+border-radius: 8px;
+max-width: 100%;
+overflow-x: auto;
+}
 </style>
